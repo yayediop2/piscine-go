10ddbf345032bd60ca10560e72b46e5d47ab552f
pndione <djibyndione26@gmail.com>
2023-01-12 16:17:07 +0000

    negatif ou positif

--- isnegative.go
+++ isnegative.go
@@ -4,10 +4,10 @@ import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb < 0 {
-		z01.PrintRune('F')
+		z01.PrintRune('T')
 		z01.PrintRune('\n')
 	} else {
-		z01.PrintRune('T')
+		z01.PrintRune('F')
 		z01.PrintRune('\n')
 	}
 }

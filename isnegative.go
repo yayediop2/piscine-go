bf58d25520999912a6086bdcb0937d6fede72f5f
pndione <djibyndione26@gmail.com>
2023-01-12 16:16:02 +0000

    negatif ou positif

--- isnegative.go
+++ isnegative.go
@@ -1,12 +1,12 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-	if nb <= 0 {
+	if nb < 0 {
 		z01.PrintRune('F')
 		z01.PrintRune('\n')
-	} else if nb > 0 {
+	} else {
 		z01.PrintRune('T')
 		z01.PrintRune('\n')
 	}

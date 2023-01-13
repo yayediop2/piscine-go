4fff51f61828d1e640a07e8507017931bf91a391
pndione <djibyndione26@gmail.com>
2023-01-13 12:03:38 +0000

    combinaisons 2

--- printcomb2.go
+++ printcomb2.go
@@ -7,22 +7,24 @@ func PrintComb2() {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
-					if i == '9' && j == '9' && k == '9' && l == '9' {
-						z01.PrintRune(i)
-						z01.PrintRune(j)
-						z01.PrintRune(' ')
-						z01.PrintRune(k)
-						z01.PrintRune(l)
-						z01.PrintRune('\n')
+					if 10*i+j < 10*k+l {
+						if i == '9' && j == '8' && k == '9' && l == '9' {
+							z01.PrintRune(i)
+							z01.PrintRune(j)
+							z01.PrintRune(' ')
+							z01.PrintRune(k)
+							z01.PrintRune(l)
+							z01.PrintRune('\n')
 
-					} else {
-						z01.PrintRune(i)
-						z01.PrintRune(j)
-						z01.PrintRune(' ')
-						z01.PrintRune(k)
-						z01.PrintRune(l)
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
+						} else {
+							z01.PrintRune(i)
+							z01.PrintRune(j)
+							z01.PrintRune(' ')
+							z01.PrintRune(k)
+							z01.PrintRune(l)
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
 					}
 				}
 			}

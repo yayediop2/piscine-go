efdc6b3dfa424a379c8537e26f173e189e5c9331
pndione <djibyndione26@gmail.com>
2023-01-13 10:14:22 +0000

    teacher

--- teacher.sh
+++ teacher.sh
@@ -1,4 +1,5 @@
+cd the-final-cl-test/mystery
 var_E = $(head -n 179 streets/Buckingham_Place | tail -n 1)
 echo $var_E
-echo interviews/interview"$var_E"
+echo interviews/interview-"$var_E"
 echo $MAIN_SUSPECT 

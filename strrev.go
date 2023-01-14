dadb0b3ed641c5d59be78f1e861071e7e443b2db
pndione <djibyndione26@gmail.com>
2023-01-14 19:25:16 +0000

    String reverse

---
+++ strrev.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func StrRev(s string) string {
+	var t string
+	for i := len(s) - 1; i >= 0; i-- {
+		t += string(s[i])
+	}
+	return t
+}
+
+func main() {
+	s := "Hello World!"
+	s = StrRev(s)
+	fmt.Println(s)
+}

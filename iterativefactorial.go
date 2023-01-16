ad4a99f5846235d3fba7af890c376aa4a391b4e7
pndione <djibyndione26@gmail.com>
2023-01-16 15:02:18 +0000

    My factoriel

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,10 +3,10 @@ package piscine
 func IterativeFactorial(nb int) int {
 	var r int = 1
 
-	if nb == 0 || nb > 100 {
+	if nb == 0 {
 		return 1
 	}
-	if nb < 0 {
+	if nb < 0 || nb > 20 {
 		return 0
 	}
 	if nb > 0 {

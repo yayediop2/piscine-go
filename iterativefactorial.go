0d0b7a7efcf5bece7ec4a0ee0c5c19d5a8868055
pndione <djibyndione26@gmail.com>
2023-01-16 14:19:22 +0000

    My factoriel

---
+++ iterativefactorial.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	var r int = 1
+	if nb == 0 {
+		return 1
+	}
+	if nb > 0 {
+		for i := 1; i <= nb; i++ {
+			r = r * i
+		}
+	}
+	return int(r)
+}

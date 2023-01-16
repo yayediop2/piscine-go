e579125d89c17654c04aa768333df81084cc3eab
pndione <djibyndione26@gmail.com>
2023-01-16 17:34:49 +0000

    My iterative power

---
+++ iterativepower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	var r int = 1
+	if nb < 0 || power < 0 {
+		return 0
+	}
+
+	for i := 1; i <= power; i++ {
+		r = r * nb
+	}
+	return r
+}

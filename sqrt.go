37e1193a0ca783f62ea4794058d5b63692bbb605
pndione <djibyndione26@gmail.com>
2023-01-17 10:09:01 +0000

    Racine carrée

---
+++ sqrt.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Sqrt(nb int) int {
+	var r int
+	if nb < 0 {
+		return 0
+	}
+	if nb == 1 {
+		return 1
+	}
+
+	for i := 1; i <= nb; i++ {
+		if i*i == nb {
+			r = i
+		}
+	}
+	return r
+}

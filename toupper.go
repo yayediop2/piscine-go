290ae7453f44d103be97333b61ccdece72fa2dce
Yaye Fatou Diop <yayediop@noreply.learn.zone01dakar.sn>
2023-01-18 18:35:25 +0000

    Update 'toupper.go'

--- toupper.go
+++ toupper.go
@@ -1,10 +1,10 @@
 package piscine
 
 func ToUpper(s string) string {
-	bs := []byte(s)
-	for _, value := range bs {
+	bs := []rune(s)
+	for i, value := range bs {
 		if value >= 97 && value <= 122 {
-			value = value - 32
+			bs[i] -= 32
 		}
 	}
 	return string(bs)

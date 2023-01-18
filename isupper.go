f469d465b6a85588dd20faaf3ba7b6a4dbcfd2c2
Yaye Fatou Diop <yayediop@noreply.learn.zone01dakar.sn>
2023-01-18 16:51:35 +0000

    Update 'isupper.go'

--- isupper.go
+++ isupper.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package piscine
 
 func IsUpper(s string) bool {
 	bs := []byte(s)
@@ -10,9 +8,4 @@ func IsUpper(s string) bool {
 		}
 	}
 	return true
-}
-
-func main() {
-	fmt.Println(IsUpper("HELLO"))
-	fmt.Println(IsUpper("HELLO!"))
 }

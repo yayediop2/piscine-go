f9ce35d580e922528dac87d4dc809b1237016363
yayediop <mamyta2000@outlook.fr>
2023-01-23 12:53:02 +0000

    point1

--- point/main.go
+++ point/main.go
@@ -1,17 +1,36 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/01-edu/z01"
 
 type point struct {
 	x int
 	y int
 }
 
+func Print_int(a int) {
+	nouveauN := a / 10
+	if nouveauN != 0 {
+		Print_int(nouveauN)
+	}
+	result := a % 10
+	z01.PrintRune(rune(result) + '0')
+}
+
 func main() {
 	points := point{}
 	points.x = 42
 	points.y = 21
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	Print_int(points.x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	Print_int(points.y)
+	z01.PrintRune('\n')
 }

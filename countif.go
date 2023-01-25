96f926bf849fdfe421829775300f37cf9fc313cd
yayediop <mamyta2000@outlook.fr>
2023-01-25 12:29:44 +0000

    count if

--- countif.go
+++ countif.go
@@ -4,11 +4,7 @@ func CountIf(f func(string) bool, tab []string) int {
 	x := 0
 	for _, v := range tab {
 		if f(v) {
-			for _, value := range v {
-				if value >= '0' && value <= '9' {
-					x = x + 1
-				}
-			}
+			x = x + 1
 		}
 	}
 	return x

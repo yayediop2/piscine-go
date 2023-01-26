25e73671644afb843943512c4fc5b816691286c1
yayediop <mamyta2000@outlook.fr>
2023-01-26 15:07:35 +0000

    hackaton 3

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -1,21 +1,17 @@
 package piscine
 
-func CollatzCountdown(start int) int {
+func CollatzCountDown(start int) int {
 	c := 0
 	if start <= 0 {
 		return -1
-	} else {
-		for i := 0; i <= start; i++ {
-			if start%2 == 0 {
-				start = start / 2
-				c++
-			}
-			if start%2 == 1 {
-				c++
-				start = (start * 3) + 1
-				c++
-			}
+	}
+	for start > 1 {
+		if start%2 == 0 {
+			start = (start / 2)
+		} else if start%2 == 1 {
+			start = ((start * 3) + 1)
 		}
+		c++
 	}
 	return c
 }

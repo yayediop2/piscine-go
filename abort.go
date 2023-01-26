0215fedc8cbcaf2a1fab258d89627f36a161d159
yayediop <mamyta2000@outlook.fr>
2023-01-26 13:47:24 +0000

    hackaton 2

---
+++ abort.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Abort(a, b, c, d, e int) int {
+	v := []int{a, b, c, d, e}
+	var z int
+	for i := 0; i < len(v); i++ {
+		for j := i + 1; j < len(v); j++ {
+			if v[i] > v[j] {
+				v[i], v[j] = v[j], v[i]
+				z = v[2]
+			}
+		}
+	}
+	return z
+}
+
+func main() {
+	middle := Abort(-2, -3, -889654, -674875, -7)
+	fmt.Println(middle)
+}

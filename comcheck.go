b02e525270b4c4704785f6d34c46007821d510f2
yayediop <mamyta2000@outlook.fr>
2023-01-26 15:48:52 +0000

    hackaton 4

---
+++ comcheck.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arg := os.Args[1:]
+	for _, v := range arg {
+		if v == "galaxy" || v == "01" || v == "galaxy 01" {
+			com := "Alert!!!"
+			for _, val := range com {
+				z01.PrintRune(val)
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}

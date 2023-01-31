f89a580cbf0cfcbac37436dd69c6c68f979322f0
yayediop <mamyta2000@outlook.fr>
2023-01-31 23:11:17 +0000

    Q11

--- listpushback.go
+++ listpushback.go
@@ -12,8 +12,8 @@ type List struct {
 
 func ListPushBack(l *List, data interface{}) {
 	n := &NodeL{Data: data}
-	if l.Head == nil {
-		l.Head = n
+	if l.Tail == nil {
+		l.Tail = n
 	} else {
 		l.Tail.Next = n
 		l.Tail = n

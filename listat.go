d8f8edd54205f0fe25c97eef9f3dd60ea7df0c60
yayediop <mamyta2000@outlook.fr>
2023-02-01 05:44:37 +0000

    Q11

--- listat.go
+++ listat.go
@@ -3,10 +3,14 @@ package piscine
 func ListAt(l *NodeL, pos int) *NodeL {
 	if l.Next == nil && pos > 0 {
 		return nil
-	}
-	for l != nil {
-		l = l.Next
-		pos--
+	} else {
+		for pos != 0 {
+			if l.Next == nil {
+				return nil
+			}
+			l = l.Next
+			pos--
+		}
 	}
 	return l
 }

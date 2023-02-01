05e852acebf784f5235701c7ce734bd3bd3db310
yayediop <mamyta2000@outlook.fr>
2023-02-01 02:08:01 +0000

    Q11

--- listlast.go
+++ listlast.go
@@ -1,32 +1,8 @@
-package main
-
-import "fmt"
-
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+package piscine
 
 func ListLast(l *List) interface{} {
 	if l.Head == nil {
 		return nil
 	}
 	return l.Tail.Data
-}
-
-func main() {
-	link := &List{}
-	link2 := &List{}
-
-	ListPushBack(link, "three")
-	ListPushBack(link, 3)
-	ListPushBack(link, "1")
-
-	fmt.Println(ListLast(link))
-	fmt.Println(ListLast(link2))
 }

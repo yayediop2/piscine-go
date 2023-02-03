90121f9ebe6a7dc94e04b1fcf9ec8e3e70d5b9e8
yayediop <mamyta2000@outlook.fr>
2023-02-03 17:06:13 +0000

    q11

--- listreverse.go
+++ listreverse.go
@@ -1,16 +1,4 @@
-package main
-
-import "fmt"
-
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+package piscine
 
 func ListReverse(l *List) {
 	var nod *NodeL = nil // c'est à dire qu'elle ne pointe vers auncun noeud
@@ -22,36 +10,4 @@ func ListReverse(l *List) {
 	}
 	l.Tail = l.Head
 	l.Head = nod
-}
-
-func ListPushBack(l *List, data interface{}) {
-	n := &NodeL{Data: data}
-	if l.Head == nil {
-		l.Head = n
-		l.Tail = n
-	} else {
-		l.Tail.Next = n
-		l.Tail = n
-	}
-}
-
-func main() {
-	link := &List{}
-
-	ListPushBack(link, 1)
-	ListPushBack(link, 2)
-	ListPushBack(link, 3)
-	ListPushBack(link, 4)
-
-	ListReverse(link)
-
-	it := link.Head
-
-	for it != nil {
-		fmt.Println(it.Data)
-		it = it.Next
-	}
-
-	fmt.Println("Tail", link.Tail)
-	fmt.Println("Head", link.Head)
 }

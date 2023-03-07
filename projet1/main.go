fc732f5c592837a7bee805132e7f5cac8c0f1a96
yayediop <mamyta2000@outlook.fr>
2023-03-07 11:19:50 -0800

    proj

--- projet1/main.go
+++ projet1/main.go
@@ -2,30 +2,79 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func main() {
-	//hex
-func hexadecimal(s string) int{
-	var n=0
-	for i:=0; i<len(s); i++{
-		n*=16
-		if s[i]>='0' && s[i]<='9'{
-			n+=int(s[i]-'0')
-		} else if s[i]>='a' && s[i]<='z'{
-			n+=int(s[i]-'a'+10)
-		}else if s[i]>='A' && s[i]<='Z'{
-			n+=int(s[i]-'A'+10)
+// hex
+func hexadecimal(s string) int {
+	var n = 0
+	for i := 0; i < len(s); i++ {
+		n *= 16
+		if s[i] >= '0' && s[i] <= '9' {
+			n += int(s[i] - '0')
+		} else if s[i] >= 'a' && s[i] <= 'z' {
+			n += int(s[i] - 'a' + 10)
+		} else if s[i] >= 'A' && s[i] <= 'Z' {
+			n += int(s[i] - 'A' + 10)
 		}
 	}
 	return n
 }
 
-func 
-	/*a, _ := strconv.ParseInt("1E", 16, 32)
-	//bin
-	b, _ := strconv.ParseInt("11", 2, 32) */
-	fmt.Println(b)
+// bin
+func binaire(s string) int {
+	n := 0
+	for _, l := range s {
+		n *= 2
+		if l == '0' || l == '1' {
+			n += int(l - '0')
+		}
+	}
+	return n
+}
+
+// cap
+func Capitalize(s string) string {
+	sr := []rune(s)
+	if len(s) > 0 {
+		for i := 0; i < len(s); i++ {
+			if sr[i] == ' ' {
+				sr[i+1] = (sr[i+1] + ' ')
+			}
+		}
+	}
+	return string(sr)
+}
+
+// up
+func ToUpper(s string) string {
+	bs := []rune(s)
+	for i, value := range bs {
+		if value >= 97 && value <= 122 {
+			bs[i] -= 32
+		}
+	}
+	return string(bs)
+}
+
+// low
+func ToLower(s string) string {
+	bs := []rune(s)
+	for i, value := range bs {
+		if value >= 65 && value <= 90 {
+			bs[i] += 32
+		}
+	}
+	return string(bs)
+}
+
+/*
+a, _ := strconv.ParseInt("1E", 16, 32)
+//bin
+b, _ := strconv.ParseInt("11", 2, 32)
+*/
+func main() {
+	a := binaire("11")
+	b := hexadecimal("1E")
 	fmt.Println(a)
+	fmt.Println(b)
 }

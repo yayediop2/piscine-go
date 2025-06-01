fe6f16aa6f2a7ae9a7f5821a240704b17cc7dccf
yayediop2 <yayefatoudiop2000@gmail.com>
2025-06-01 23:59:10 +0000

    transfer from gitea

---
+++ commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Ajouter les dépôts distants
+ git remote add origin https://learn.zone01dakar.sn/git/yayediop/piscine-flutter
+ git remote add github https://github.com/yayediop2/piscine-flutter
+
+# Ajouter tous les fichiers au commit
+git add .
+
+# Faire un commit avec un message spécifié par l'utilisateur
+echo "Entrez votre message de commit: "
+read commit_message
+git commit -m "$commit_message"
+
+# Pousser les changements au dépôt par défaut (origin)
+git push
+
+# Renommer la branche master en main
+git branch -m master main
+
+# Pousser la branche main sur GitHub
+git push github main 
+
+# Renommer la branche master localement 
+git branch -m master
